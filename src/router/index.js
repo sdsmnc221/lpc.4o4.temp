f2a21ad01a5def39f51055fc1c014312c056cead
sdsmnc221 <antr.2201@gmail.com>
2024-11-24 18:29:48 +0100

    feat(app): meta title

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,9 @@ const routes = [
     path: "/adoptions/snoop-tagada",
     name: "Adoption de Snoop & Tagada",
     component: Adoptions_SnoopTagada,
+    meta: {
+      title: "Adoption chez Les Petits Clochards : Notre duo Snoop & Tagada",
+    },
   },
   {
     path: "/:pathMatch(.*)*",
@@ -29,6 +32,12 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+// Navigation guard to handle title changes
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title || "Les Petits Clochards 91 - En construction"; // Fallback title if none specified
+  next();
 });
 
 export default router;
